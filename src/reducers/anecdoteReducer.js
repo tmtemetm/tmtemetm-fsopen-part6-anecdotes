f3c6a61e587e5a76223671754b4b7a607e34057d
tmtemetm <teemu.j.maki@aalto.fi>
2024-05-25 17:09:14 +0300

    Tehtävä 6.14

--- src/reducers/anecdoteReducer.js
+++ src/reducers/anecdoteReducer.js
@@ -1,14 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
-
 const getId = () => (100000 * Math.random()).toFixed(0)
 
 const asObject = (anecdote) => {
@@ -19,27 +10,28 @@ const asObject = (anecdote) => {
   }
 }
 
-const initialState = anecdotesAtStart.map(asObject)
-
 const anecdoteSlice = createSlice({
   name: 'anecdotes',
-  initialState,
+  initialState: [],
   reducers: {
     createAnecdote(state, { payload }) {
-      state.push({
-        content: payload,
-        id: getId(),
-        votes: 0
-      })
+      state.push(asObject(payload))
     },
     voteAnecdote(state, { payload }) {
       return state.map(anecdote => anecdote.id === payload
         ? { ...anecdote, votes: anecdote.votes + 1 }
         : anecdote)
         .sort((a1, a2) => a2.votes - a1.votes)
+    },
+    setAnecdotes(state, { payload }) {
+      return payload
     }
   }
 })
 
-export const { createAnecdote, voteAnecdote } = anecdoteSlice.actions
+export const {
+  createAnecdote,
+  voteAnecdote,
+  setAnecdotes
+} = anecdoteSlice.actions
 export default anecdoteSlice.reducer
